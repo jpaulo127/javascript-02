f8bb121b7938d675332ca0e0426d64a750d15d14
joao paulo Oliveira <joaopaulooliveira102@gmail.com>
2023-06-03 20:15:54 -0300

    exercicio09

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const user01 = new object()
+user01.id = 1
+user01.nome = "jaõa"
+user01.idade = 25
+
+const user02 = new object()
+user02.id = 2
+user02.nome = "pedro"
+user02.idade = 30
+
+const user02 = new object()
+user03.id = 3
+user03.nome = "maria"
+user03.idade = 28
---
+++ src/arrowFunction.js
@@ -0,0 +1,8 @@
+function pessoa() {
+    this.idade =0
+    
+    setInterval(() => {
+        console.log(this.idade++)   
+    }, 2000)
+}
+const p1 = new pessoa()
